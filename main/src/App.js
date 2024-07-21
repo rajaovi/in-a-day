8ca04a7e7c772a47293818ce6cf00c82f86dee1f
Raja <orajacse@gmail.com>
2024-07-21 21:25:02 +0530

    SCSS Initial Commit

--- main/src/App.js
+++ main/src/App.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { getAllPosts } from './store/Actions/TestAction';
 import Signup from './pages/Signup';
+import './styles/_common.scss';
 
 function App() {
   const dispatch = useDispatch();
